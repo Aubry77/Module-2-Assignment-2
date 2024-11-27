dcdc6a6a77ec79ba46d170cd4592bc540b11afac
aubry77 <aubreyletson77@gmail.com>
2024-11-27 03:13:36 -0600

    mod 2 assign 4 1st commit

---
+++ module 2 assignment 4  list.py
@@ -0,0 +1,10 @@
+"""_summary_list transformation task 1
+"""
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+
+# Sort the list in descending order
+grades.sort(reverse=True)
+
+# Print the sorted list
+print("Sorted grades:", grades)
